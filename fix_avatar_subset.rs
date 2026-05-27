6bf4bc8e8cdbd821e911a1f53d71741f37436aa1
Olivier ROLAND <newca12@users.noreply.github.com>
2026-05-27 11:09:15 +0000

    chore: remove stray debug files from workspace root

--- fix_avatar_subset.rs
+++
@@ -1,3 +0,0 @@
-fn is_subset(a: &[u32], b: &[u32]) -> bool {
-    a.iter().all(|x| b.contains(x))
-}
--- run_syn.sh
+++
@@ -1,1 +0,0 @@
-cargo run --release --bin mrs -- crates/mrs-bench/problems/casc-30/FNE/SYN457+1.p --time 5
